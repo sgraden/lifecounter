778d5a77bc2d18343679b3126939972136a408b1
Steven Raden <sraden@uw.edu>
2015-01-27 16:07:28 -0800

    Rotate and incrementing working

--- app/src/main/java/edu/washington/sraden/lifecounter/LifeCounter.java
+++ app/src/main/java/edu/washington/sraden/lifecounter/LifeCounter.java
@@ -1,40 +1,54 @@
 package edu.washington.sraden.lifecounter;
 
-import android.nfc.Tag;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class LifeCounter extends ActionBarActivity {
     private static final String TAG = "LifeCounterMain";
-    private int numberOfPlayers;
+    private ArrayList<Integer> playerInfo = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_counter);
 
-        if (savedInstanceState != null) {
-            numberOfPlayers = savedInstanceState.getInt("numPlayers");
-            for (int i = 0; i < numberOfPlayers; i++) {
-                
+        if (savedInstanceState == null) {
+            final int DEFAULT = 4;
+            for (int i = 0; i < DEFAULT; i++) {
+                playerInfo.add(i, 20);
+
+                String playerID = "player" + (i + 1);
+                int playerTableID = getResources().getIdentifier(playerID, "id", getPackageName());
+                TableLayout playerTable = (TableLayout) findViewById(playerTableID);
+
+                TextView playerTitle = (TextView) playerTable.findViewById(R.id.player_name);
+
+                String playerTitleText =  getResources().getString(R.string.player_name);
+                playerTitle.setText(String.format(playerTitleText, i + 1)); // Sets name of player
+
+                TextView healthView = (TextView) playerTable.getChildAt(2).findViewById(R.id.player_health);
+                healthView.setText("" + playerInfo.get(i)); //Set the health to value from arraylist
             }
         }
-
-
     }
 
     public void clickFunction(View v) {
-        TableLayout player = (TableLayout) v.getParent().getParent();
-        TextView health = (TextView) player.getChildAt(2).findViewById(R.id.player_health);
-        int currHealth = Integer.parseInt(health.getText().toString());
+        TableLayout player = (TableLayout) v.getParent().getParent(); //Player view
+        String playerID = v.getResources().getResourceName(player.getId()); //String of player id
+        int playerNum = Integer.parseInt(playerID.substring(playerID.length() -1)) - 1; //Integer value for player space
+
+        TextView health = (TextView) player.getChildAt(2).findViewById(R.id.player_health); //Textview of the health
+        int currHealth = playerInfo.get(playerNum); //Current health based on arraylist
+
         switch (v.getTag().toString()) {
             case "Pos1": currHealth += 1;
                 break;
@@ -49,10 +63,38 @@ public class LifeCounter extends ActionBarActivity {
             currHealth = 0;
         }
         health.setText("" + currHealth);
+        playerInfo.set(playerNum, currHealth);
     }
 
     protected void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        saveInstanceState.putInt("numPlayers", playerInfo.size());
+        for (int i = 0; i < playerInfo.size(); i++) {
+            int playerScore = playerInfo.get(i);
+            String playerID = "player" + (i + 1);
+            saveInstanceState.putInt(playerID, playerScore);
+        }
+    }
 
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        for (int i = 0; i < savedInstanceState.getInt("numPlayers"); i++) {
+            String playerID = "player" + (i + 1); //String ID of player table
+            playerInfo.add(i, savedInstanceState.getInt(playerID)); //Add health to the array
+            int playerTableID = getResources().getIdentifier(playerID, "id", getPackageName()); //Int ID of player table
+
+            TableLayout playerTable = (TableLayout) findViewById(playerTableID); //Root table of player
+
+            //Set name of player
+            TextView playerTitle = (TextView) playerTable.findViewById(R.id.player_name); //Title name of player
+            String playerTitleText = getResources().getString(R.string.player_name); //String format of title name
+            playerTitle.setText(String.format(playerTitleText, i + 1)); // Sets name of player
+
+            //Set health value of player
+            TextView healthView = (TextView) playerTable.getChildAt(2).findViewById(R.id.player_health);
+            healthView.setText("" + playerInfo.get(i)); //Set the health to value from playerInfo array
+        }
     }
 
     @Override
